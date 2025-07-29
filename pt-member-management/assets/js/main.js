eefe1fbfbf873e5f1d2b6e85c6f427cf1edc3146
Mingich <105227193+Mingich@users.noreply.github.com>
2025-07-29 09:48:52 +0900

    Delete pt-member-management directory

--- pt-member-management/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = "https://script.google.com/macros/s/APP_SCRIPT_URL/exec";
-
-document.getElementById("registerForm").addEventListener("submit", e => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  fetch(scriptURL, { method: "POST", body: formData })
-    .then(() => alert("회원 정보가 등록되었습니다!"))
-    .catch(err => alert("오류 발생: " + err.message));
-});
